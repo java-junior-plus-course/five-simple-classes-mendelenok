73ba71c19fa263398af47e6d47d60f62b068bb36
Dmitri_Mendelev <dmitri.mendelev@gmail.com>
2020-11-19 02:47:44 +0300

    Corrected Multiplication class with setters and getters

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -1,15 +1,22 @@
 public class Multiplication {
-    private double term;
+    private double term1;
+    private double term2;
 
-    public double getTerm() {
-        return term;
+    public double getTerm1() {
+        return term1;
     }
 
-
-    public void setTerm(double term) {
-        this.term = term;
+    public double getTerm2() {
+        return term2;
     }
 
+    public void setTerm1(double term) {
+        this.term1 = term;
+    }
+
+    public void setTerm2(double term) {
+        this.term2 = term;
+    }
 
     public double doMult (double term1, double term2) {
         System.out.print("Result of multiplication is: ");
