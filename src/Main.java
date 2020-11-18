52a2856f0b4ed4c884ccfe6299a54821877eb411
Dmitri_Mendelev <dmitri.mendelev@gmail.com>
2020-11-19 02:42:19 +0300

    Corrected Main class with setters and getters to Summation class objects

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //not using in stream
+        var1 = 4.7;
+        var2 = 5.9;
+
         Summation sum = new Summation();
-        System.out.println(sum.doSum(var1 = 4.7,var2 = 5.6));
+        sum.setTerm1(var1);
+        sum.setTerm2(var2);
+        System.out.println(sum.doSum(sum.getTerm1(), sum.getTerm2()));
 
         Substraction subs = new Substraction();
         System.out.println(subs.doSubs(var1 = 4.7,var2 = 5.6));
