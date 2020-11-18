ea892175da3f7d0ca3178eb49375403e14572fee
Dmitri_Mendelev <dmitri.mendelev@gmail.com>
2020-11-18 16:33:58 +0300

    Created Multiplication class

---
+++ src/Multiplication.java
@@ -0,0 +1,18 @@
+public class Multiplication {
+    private int term;
+
+    public int getTerm() {
+        return term;
+    }
+
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+
+    public int doMult (int term1, int term2) {
+        System.out.print("Result of substraction is: ");
+        return (term1 * term2);
+    }
+}
