7a21f6d6c8e5b97fbb69d71ff7e1ae8aba50b095
Dmitri_Mendelev <dmitri.mendelev@gmail.com>
2020-11-18 16:43:04 +0300

    Division class is done

--- src/Division.java
+++ src/Division.java
@@ -1,2 +1,18 @@
 public class Division {
+    private int term;
+
+    public int getTerm() {
+        return term;
+    }
+
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+
+    public int doDiv (int term1, int term2) {
+        System.out.print("Result of division is: ");
+        return (term1 % term2);
+    }
 }
