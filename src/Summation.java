6a0c097d87fddb830098cdf25eb214bf73de2005
Dmitri_Mendelev <dmitri.mendelev@gmail.com>
2020-11-18 16:15:01 +0300

    Created class Summation

---
+++ src/Summation.java
@@ -0,0 +1,18 @@
+public class Summation {
+    private int term;
+
+    public int getTerm() {
+        return term;
+    }
+
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+
+    public int doSum (int term1, int term2) {
+        System.out.print("Result of summation is: ");
+        return (term1 + term2);
+    }
+}
