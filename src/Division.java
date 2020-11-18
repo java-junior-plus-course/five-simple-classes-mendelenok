6e8ab4f2a84c60c27a593467a9a9d2b3453a3b02
Dmitri_Mendelev <dmitri.mendelev@gmail.com>
2020-11-19 02:49:21 +0300

    Corrected Division class with setters and getters

--- src/Division.java
+++ src/Division.java
@@ -1,13 +1,22 @@
 public class Division {
-    private double term;
+    private double term1;
+    private double term2;
 
-    public double getTerm() {
-        return term;
+    public double getTerm1() {
+        return term1;
+    }
+
+    public double getTerm2() {
+        return term2;
     }
 
 
-    public void setTerm(double term) {
-        this.term = term;
+    public void setTerm1(double term) {
+        this.term1 = term;
+    }
+
+    public void setTerm2(double term) {
+        this.term2 = term;
     }
 
 
